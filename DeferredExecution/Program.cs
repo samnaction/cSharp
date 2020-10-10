786670f7ef80b56d529952142e8895764678f3c6
Sameer Nazeer <sameernln@gmail.com>
2020-10-10 17:26:07 +0530

    Streaming and non streaming operator

--- DeferredExecution/Program.cs
+++ DeferredExecution/Program.cs
@@ -8,6 +8,18 @@ namespace DeferredExecution
     {
         static void Main(string[] args)
         {
+            var numbers = Random().Where(n => n > 0.5).Take(5).OrderByDescending( n => n);
+
+            //Infinit Loop
+            //var numbers = Random().Where(n => n > 0.5).OrderByDescending(n => n).Take(5);
+
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"Number::{number}");
+            }
+
+
             List<Movie> movies = new List<Movie>()
             {
                 new Movie (2010, "Movie A"),
@@ -43,6 +55,24 @@ namespace DeferredExecution
                 Console.WriteLine($"Name: {movie.Name}");
             }
 
+            Console.WriteLine("**********************************");
+
+            var query4 = movies.FilterWithYield(m => m.Year > 2010).Take(1);
+
+            var enumerator = query.GetEnumerator();
+
+            //foreach (var movie in query4)
+            //{
+            //    Console.WriteLine($"Name: {movie.Name}");
+            //}
+
+        }
+
+        public static IEnumerable<double> Random()
+        {
+            var random = new Random();
+            while (true)
+                yield return random.NextDouble();
         }
     }
 }
