f6d56ef4e3292ea9bbdccf259f28f64d8e91a889
samnaction <sameernln@gmail.com>
2020-11-03 12:57:10 +0530

    Async Void
    Co-Authored-By: samnaction <7768320+samnaction@users.noreply.github.com>

---
+++ AsyncVoid/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncVoid
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+               AsyncVoid();
+
+                Console.WriteLine("Hello World!");
+
+                await AsyncTask();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static async void AsyncVoid()
+        {
+            
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:50000");
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    
+                }
+            }
+        }
+
+
+        static async Task AsyncTask()
+        {
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:50000");
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+            }
+        }
+
+    }
+}
